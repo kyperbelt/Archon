6f7ee632a821f8bdaeeaf1367900c9ad6c1fbd98
Kyperbelt <jonathancamarenacamcho@gmail.com>
2018-04-27 18:03:28 -0700

    Initial commit

---
+++ Game1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using Archon.engine;
+
+namespace Archon
+{
+    /// <summary>
+    /// this is the main type for your game.
+    /// </summary>
+    public class Game1 : ArchonGame
+    {
+
+        public Game1() : base(320,320){
+        }
+
+        public override void archonCreate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void archonResize(int width, int height)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void archonDraw(float delta)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void archonUpdate(float delta)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void archonDispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ engine/ArchonGame.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Archon.engine
+{
+    public abstract class ArchonGame : Game
+    {
+
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D texture;
+
+        public ArchonGame(int width,int height)
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.IsFullScreen = false;
+            graphics.PreferredBackBufferWidth = width;
+            graphics.PreferredBackBufferHeight = height;
+            Window.AllowUserResizing = true;
+
+            Content.RootDirectory = "assets";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            var filePath = Path.Combine(Content.RootDirectory, "textures/WeinerCon.png");
+
+            using (var stream = TitleContainer.OpenStream(filePath))
+            {
+                texture = Texture2D.FromStream(graphics.GraphicsDevice,stream);
+            }
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture,new Vector2(0,0),Color.White);
+            spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+
+        public virtual void archonPaused() { }
+        public virtual void archonResumed() { }
+
+        /**
+         * archon game should be created here
+         */
+        public abstract void archonCreate();
+        /**
+         * archon game window was resized
+         */
+        public abstract void archonResize(int width, int height);
+        /**
+         * archon rendering all logic and rendering is done in here
+         */
+        public abstract void archonDraw(float delta);
+
+        /**
+        *
+        * archon update
+        */
+        public abstract void archonUpdate(float delta);
+        /**
+         * 
+         *dispose of all your archon assets 
+         */
+        public abstract void archonDispose();
+    }
+}
